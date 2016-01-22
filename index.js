b819cdf906e9e12326240b44076024a537f86bde
Chris Roberson <chrisronline@gmail.com>
2016-01-22 13:23:36 -0500

    wip

---
+++ index.js
@@ -0,0 +1,17 @@
+import find from './lib/find'
+import getManager from './lib/managers'
+
+async function run() {
+  const packages = await find()
+    
+  for (let pkg of packages) {
+    const manager = getManager(pkg.name)
+    const updates = await manager.getUpdates(pkg)
+  }
+  
+  return 'Done'
+}
+
+run()
+  // .then((msg) => console.log(msg))
+  // .catch((e) => console.warn(e))
